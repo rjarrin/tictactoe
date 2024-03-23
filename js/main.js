0254d32f560108a532435fa865c3de3827e3887c
rjarrin <jarrin.ricardo@gmail.com>
2024-03-23 17:12:57 -0400

    Check winning conditions implemented

--- js/main.js
+++ js/main.js
@@ -26,6 +26,33 @@ function handleCellClick(e) {
     }
 }
 
+function checkWin(player) {
+    const playerClass = "player-${player}";
+    // Define winning row outcomes
+    const rows = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+    ];
+    // Define the winning column outcomes
+    const columns = [
+        [0, 3, 6],
+        [1, 4, 7], 
+        [2, 5, 8]
+    ];
+    // Define the winning diagonal outcomes
+    const diagonals = [
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    // Check if any of the outcomes were matched by an individual player
+    let checker = rows.some(row => row.every(index => cells[index].classList.contains(playerClass))) || 
+    columns.some(column => column.every(index => cells[index].classList.contains(playerClass))) ||
+    diagonals.some(diagonal => diagonal.every(index => cells[index].classList.contains(playerClass)));
+
+    return checker;
+}
+
 function placeMark(cell, player) {
     cell.classList.add("player-${player}");
     cell.textContent = player;
